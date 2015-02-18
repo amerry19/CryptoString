cabe8705a32e89567bf182b500f1c0978ffdc0b9
Adam Merry <adam.martin.merry@gmail.com>
2015-02-18 00:05:04 -0800

    Added text cloaking and replaced window prompt with inline display of hash within extension popup.

--- cryptopass.js
+++ cryptopass.js
@@ -1,9 +1,75 @@
-$("#input").keyup(function (event) {
-  if (event.keyCode === 13) {
-      var $inputValue = $(this).val();
-      var hash = CryptoJS.SHA3($inputValue, { outputLength: 512 }).toString();
-      var middle = Math.floor(hash.length / 4);
-      var shortHash = hash.substr(0, middle);
-      window.top.prompt("Encrypted!" + "\n\n" + "Copy to clipboard: Ctrl+C, Enter", shortHash);
-    }   
+$("#textToggle").click(function (event) {
+	CloakText();
 });
+
+if ($("#textToggle:checked")){
+	CloakText();
+}
+
+function Hash(){
+	$("#input").keyup(function (event) {
+	  if (event.keyCode === 13) {
+	      var $inputValue = $(this).val();
+	      var $output = $("#output");
+	      if ($inputValue.length < 1){
+	      	ToggleResult(false);
+	      } else {
+	        var hash = CryptoJS.SHA3($inputValue, { outputLength: 512 }).toString();
+	        var middle = Math.floor(hash.length / 4);
+	        var shortHash = hash.substr(0, middle);
+	        
+	        ToggleResult(true);
+	        $output.prepend(shortHash);
+	        SelectText("output");
+	      }
+	    }   
+	});
+}
+Hash();
+
+function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
+function CloakText() {
+  var checked = $("#textToggle:checked").length;
+  var text = $("#input").val();
+  if (checked === 1) {
+  	var type = "password";
+  	ReplaceInput(text, type);
+  }else{
+  	var type = "text";
+  	ReplaceInput(text, type);
+  }
+  // Reassign focus to input
+  $("#input").focus();
+  // Move cursor to end of string after refocusing
+  $("#input").val($("#input").val());
+};
+
+function ToggleResult(flag) {
+	var $result = $(".result");
+	$("#output").empty();
+	flag ? $result.show() : $result.hide();
+}
+
+function ReplaceInput(text, typeVal) {
+	var element = "<input id=\"input\" autofocus size=\"50\" type=\"" + typeVal + "\" value=\"" + text + "\" />";
+	$("#input").remove();
+	$("#inputPlaceholder").append(element);
+	Hash();
+}
